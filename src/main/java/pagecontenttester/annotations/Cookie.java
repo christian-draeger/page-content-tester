7fdb5407e3ad5cc7cf0013f6b1912698bbc0c1cf
Christian Dräger <christian.draeger@idealo.de>
2017-06-12 21:04:33 +0200

    target cookie annotation to type parameter

--- src/main/java/pagecontenttester/annotations/Cookie.java
+++ src/main/java/pagecontenttester/annotations/Cookie.java
@@ -6,7 +6,7 @@ import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
 @Retention(RetentionPolicy.RUNTIME)
-@Target({ ElementType.METHOD })
+@Target({ ElementType.PARAMETER })
 public @interface Cookie {
 
     String name() default "1e97fa4a-34d3-11e7-a919-92ebcb67fe33";
