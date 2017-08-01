ac5a8faa2b71a194fb2360d3aca74de246c1f857
Christian Dräger <christian.draeger@idealo.de>
2017-08-01 18:06:24 +0200

    simplify paco runner

--- src/main/java/pagecontenttester/runner/PacoRunner.java
+++ src/main/java/pagecontenttester/runner/PacoRunner.java
@@ -1,12 +1,10 @@
 package pagecontenttester.runner;
 
-import org.junit.AssumptionViolatedException;
-import org.junit.internal.runners.model.EachTestNotifier;
 import org.junit.runner.notification.RunNotifier;
-import org.junit.runner.notification.StoppedByUserException;
 import org.junit.runners.BlockJUnit4ClassRunner;
 import org.junit.runners.model.InitializationError;
-import org.junit.runners.model.Statement;
+
+import lombok.SneakyThrows;
 
 public class PacoRunner extends BlockJUnit4ClassRunner {
 
@@ -15,27 +13,9 @@ public class PacoRunner extends BlockJUnit4ClassRunner {
     }
 
     @Override
+    @SneakyThrows
     public void run(RunNotifier notifier) {
-        //Add Listener. This will register our JUnit Listener.
         notifier.addListener(new TestListener());
-
-
-        //Get each test notifier
-        EachTestNotifier testNotifier = new EachTestNotifier(notifier,
-                getDescription());
-        try {
-            //In order capture testRunStarted method
-            //at the very beginning of the test run, we will add below code.
-            //Invoke here the run testRunStarted() method
-            notifier.fireTestRunStarted(getDescription());
-            Statement statement = classBlock(notifier);
-            statement.evaluate();
-        } catch (AssumptionViolatedException e) {
-            testNotifier.fireTestIgnored();
-        } catch (StoppedByUserException e) {
-            throw e;
-        } catch (Throwable e) {
-            testNotifier.addFailure(e);
-        }
+        notifier.fireTestRunStarted(getDescription());
     }
 }
