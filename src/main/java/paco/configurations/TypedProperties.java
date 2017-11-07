0f6f778a8af77631ea2a5b6c4fdc3f8f2a66e367
Christian Dräger <christian.draeger@idealo.de>
2017-11-07 18:16:42 +0100

    fix properties fallback

--- src/main/java/paco/configurations/TypedProperties.java
+++ src/main/java/paco/configurations/TypedProperties.java
@@ -1,46 +1,50 @@
 package paco.configurations;
 
-import com.typesafe.config.Config;
-import com.typesafe.config.ConfigFactory;
-
 import lombok.extern.slf4j.Slf4j;
+
+import java.io.IOException;
+import java.util.Properties;
+
+import static java.lang.Boolean.parseBoolean;
 
 @Slf4j
 class TypedProperties {
 
-    private static TypedProperties instance;
-    private Config config;
+    private static Properties mergedProperties;
 
-    TypedProperties() {
-        config = ConfigFactory.load(TypedProperties.class.getClassLoader(), System.getProperty("paco.properties", "paco.properties"));
-    }
-
-    private static synchronized TypedProperties getInstance() {
-        if (instance == null) {
-            instance = new TypedProperties();
+    static {
+        Properties defaultProperties = new Properties();
+        try {
+            defaultProperties.load(TypedProperties.class.getResourceAsStream("/default.properties"));
+        } catch (IOException e) {
+            throw new RuntimeException(e); //NOSONAR
         }
-        return instance;
-    }
-
-
-    static String getStringValue(String key) {
-        String value = "";
-        if (!key.equals(value)) {
-            // dots are not allowed in POSIX environmental variables
-            value = System.getenv(key.replace(".", "_"));
-            if (value == null) {
-                value = TypedProperties.getInstance().config.getString(key);
-            }
-
+        mergedProperties = new Properties(defaultProperties);
+        try {
+            mergedProperties.load(TypedProperties.class.getResourceAsStream("/paco.properties"));
+        } catch (Exception e) {
+            // do nothing
         }
-        return value;
     }
 
-    static int getIntValue(String key) {
-        return TypedProperties.getInstance().config.getInt(key);
+    static String getStringValue(final String key) {
+        String customValue = System.getProperty(key, mergedProperties.getProperty(key));
+        if (customValue != null) {
+            return customValue;
+        }
+        return System.getProperty(key, mergedProperties.getProperty(key));
     }
 
-    static boolean getBooleanValue(String key) {
-        return TypedProperties.getInstance().config.getBoolean(key);
+    static int getIntValue(final String key) {
+        return Integer.parseInt(getStringValue(key));
     }
+
+    static boolean getBooleanValue(final String key) {
+        String value = getStringValue(key);
+        if (!("true".equals(value) || "false".equals(value))) {
+            throw new ConfigurationException("trying to parse non boolean value as boolean");
+        }
+        return parseBoolean(getStringValue(key));
+    }
+
 }
--- src/test/java/paco/configurations/TypedPropertiesTest.java
+++ src/test/java/paco/configurations/TypedPropertiesTest.java
@@ -1,15 +1,10 @@
 package paco.configurations;
 
-import static org.assertj.core.api.Assertions.assertThat;
-
 import org.junit.Test;
 
-public class TypedPropertiesTest {
+import static org.assertj.core.api.Assertions.assertThat;
 
-    @Test
-    public void can_load_existing_property_files() {
-        new TypedProperties();
-    }
+public class TypedPropertiesTest {
 
     @Test
     public void should_return_property_value_of_type_int() throws Exception {
@@ -30,4 +25,22 @@ public class TypedPropertiesTest {
     public void should_throw_runtime_exception() throws Exception {
         TypedProperties.getBooleanValue("paco.referrer");
     }
+
+    @Test
+    public void system_properties_overrides_paco_properties() {
+        System.setProperty("paco.foo", "override");
+        assertThat(TypedProperties.getStringValue("paco.foo")).isEqualTo("override");
+        System.setProperty("paco.foo", "42");
+        assertThat(TypedProperties.getIntValue("paco.foo")).isEqualTo(42);
+        System.setProperty("paco.foo", "false");
+        assertThat(TypedProperties.getBooleanValue("paco.foo")).isEqualTo(false);
+    }
+
+
+    @Test
+    public void paco_properties_overrides_default_properties() {
+        assertThat(TypedProperties.getStringValue("paco.ascii")).isEqualTo("false");
+        assertThat(TypedProperties.getBooleanValue("paco.ascii")).isEqualTo(false);
+    }
+
 }
