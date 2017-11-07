ca82d738ad17d13dc2461f7a703e0bad280278c9
Christian Dräger <christian.draeger1@gmail.com>
2017-11-07 21:15:33 +0100

    add some more tests for page picker

--- src/test/java/paco/fetcher/FetchSingleFromMethodTest.java
+++ src/test/java/paco/fetcher/FetchSingleFromMethodTest.java
@@ -2,9 +2,12 @@ package paco.fetcher;
 
 import org.junit.Test;
 import paco.annotations.Fetch;
+import paco.annotations.GetFetchedPageException;
 import paco.runner.Paco;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static paco.annotations.Fetch.Device.DESKTOP;
+import static paco.annotations.Fetch.Device.MOBILE;
 
 public class FetchSingleFromMethodTest extends Paco {
 
@@ -12,6 +15,30 @@ public class FetchSingleFromMethodTest extends Paco {
     @Fetch(url = "localhost/example", port = "8089")
     public void can_fetch_from_method_annotation() {
         assertThat(page.get().getTitle()).contains("i'm the title");
+    }
+
+    @Test(expected = GetFetchedPageException.class)
+    @Fetch(url = "localhost:8089/example", device = MOBILE)
+    public void throw_exeption_on_wrong_device() {
+        page.get(DESKTOP);
+    }
+
+    @Test(expected = GetFetchedPageException.class)
+    @Fetch(url = "localhost:8089/example", device = MOBILE)
+    public void throw_exeption_on_wrong_device_with_url_snippet() {
+        page.get("example", DESKTOP);
+    }
+
+    @Test(expected = GetFetchedPageException.class)
+    @Fetch(url = "localhost:8089/example", device = MOBILE)
+    public void throw_exeption_on_wrong_url_snippet_with_device() {
+        page.get("dfgfs", MOBILE);
+    }
+
+    @Test(expected = GetFetchedPageException.class)
+    @Fetch(url = "localhost:8089/example", device = MOBILE)
+    public void throw_exeption_on_wrong_url_snippet_and_wrong_device() {
+        page.get("dfgfs", DESKTOP);
     }
 
 }
